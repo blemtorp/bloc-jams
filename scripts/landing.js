31e5b4d0f02c39342836cf5705faf4ca18ef838f
Bibi Lemtorp <blemtorp@blemtorp-macbookpro.roam.corp.google.com>
2016-11-12 07:36:34 -0800

    updates to chpt9-Dom-Events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,35 +1,9 @@
-
-             // our JavaScript will go here
-             var animatePoints = function() {
+            var pointsArray = document.getElementsByClassName('point');
  
-                 var points = document.getElementsByClassName('point');
- 
-                 /* var revealFirstPoint = function() {
-                     points[0].style.opacity = 1;
-                     points[0].style.transform = "scaleX(1) translateY(0)";
-                     points[0].style.msTransform = "scaleX(1) translateY(0)";
-                     points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealSecondPoint = function() {
-                     points[1].style.opacity = 1;
-                     points[1].style.transform = "scaleX(1) translateY(0)";
-                     points[1].style.msTransform = "scaleX(1) translateY(0)";
-                     points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealThirdPoint = function() {
-                     points[2].style.opacity = 1;
-                     points[2].style.transform = "scaleX(1) translateY(0)";
-                     points[2].style.msTransform = "scaleX(1) translateY(0)";
-                     points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 revealFirstPoint();
-                 revealSecondPoint();
-                 revealThirdPoint(); */
+            var animatePoints = function(points) {
+            
                  
-                var revealPoint = function() {
+                /* var revealPoint = function() {
                     for (var i = 0; i < 3; i++ ) {
                       points[i].style.opacity = 1;
                       points[i].style.transform = "scaleX(1) translateY(0)";
@@ -40,6 +14,22 @@
                     
                 }
                 
-                revealPoint();
- 
+                revealPoint();*/
+                forEach(points);
+                 
              };
+
+            window.onload = function() {
+                if (window.innerHeight > 950) {
+                    animatePoints(pointsArray);
+                }
+                var sellingPoints = document.getElementsByClassName('selling-points')[0];
+                var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+                window.addEventListener('scroll', function(event) {
+                    //console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+                    if (document.body.scrollTop >= scrollDistance) {
+                        animatePoints(pointsArray);   
+                    }
+     });
+            }
+ 
