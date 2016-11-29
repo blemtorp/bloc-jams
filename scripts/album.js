ca2a43bcde7f8abc973dd6e42c68ef8d7b975f77
Bibi Lemtorp <blemtorp@blemtorp-macbookpro.roam.corp.google.com>
2016-11-28 21:41:43 -0800

    updates to chpt-12-play&pause

--- scripts/album.js
+++ scripts/album.js
@@ -30,6 +30,7 @@
      ]
  };
 
+// Silverback
 var silverBack = {
      title: 'The Kitty',
      artist: 'SilverBack',
@@ -48,7 +49,8 @@ var silverBack = {
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      //+ '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -58,14 +60,12 @@ var silverBack = {
  };
 
 var setCurrentAlbum = function(album) {
-     // #1
      var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
  
-     // #2
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -79,7 +79,31 @@ var setCurrentAlbum = function(album) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
- 
+
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+
+
+    songListContainer.addEventListener('mouseover', function(event) {
+      // #1
+      //console.log(event.target);
+      if (event.target.parentElement.className === 'album-view-song-item') {
+        event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+      }
+
+      
+    });
+    
+     for (var i = 0; i < songRows.length; i++) {
+             songRows[i].addEventListener('mouseleave', function(event) {
+                 // Revert the content back to the number
+                 this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+             });
+      }
+     
  };
